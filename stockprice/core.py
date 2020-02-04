16129ca1f6e8688c09206d5a2d9c0f23c0115330
Andrew Palmer <atp@sdf.org>
2020-02-04 00:18:36 -0600

    Add folders to caching paths

--- stockprice/core.py
+++ stockprice/core.py
@@ -4,28 +4,27 @@ from .cache import JsonFileCache
 from . import validation
 
 
-def cache_file(base, ticker, extension):
+class Folder(object):
+    CHART = 'chart'
+
+
+def cache_file(base, folder, ticker, extension):
     if not validation.is_valid_filename(ticker):
         raise ValueError(f'Cannot create cache file for ticker "{ticker}"')
-    return os.path.join(base, f'{ticker}.{extension}')
-
-
-def compare_close(begin, end):
-    return (end['close'] / begin['close']) - 1
-
-
-def as_percentage(value):
-    return '{}%'.format(round(value * 100, 2))
+    return os.path.join(base, folder, f'{ticker}.{extension}')
 
 
 def get_chart_data(*, ticker, cache_base):
+    def compare_close(begin, end):
+        return (end['close'] / begin['close']) - 1
+    def as_percentage(value):
+        return '{}%'.format(round(value * 100, 2))
+
     cache = JsonFileCache(
-        cache_file(cache_base, ticker, 'json'),
-        days=1,
-    )
+        cache_file(cache_base, Folder.CHART, ticker, 'json'),
+        days=1)
     values = cache.get_values(lambda: yahoo.api.chart(ticker))
     items = yahoo.get_items(values)
-
     return {
         'day': {
             'previous': items[-2],
