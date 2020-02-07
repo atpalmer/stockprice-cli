ac07262629a95c97106ef8f308953a6ad63b26e5
Andrew Palmer <atp@sdf.org>
2020-02-06 22:42:36 -0600

    Implement PE ranking

--- stockprice/aggregates.py
+++ stockprice/aggregates.py
@@ -1,7 +1,26 @@
+from collections import namedtuple
+import json
 import os
 from . import cachetools
 
 
+File = namedtuple('File', ('filename', 'contents'))
+
+
+def _filecontents(*args):
+    cache_dir = os.path.join(*args)
+    filenames = cachetools.readdir(cache_dir)
+    for filename in filenames:
+        fullpath = os.path.join(cache_dir, filename)
+        with open(fullpath) as f:
+            yield File(filename=filename, contents=json.load(f))
+
+
 def pe(cache_base):
-    cache_dir = os.path.join(cache_base, cachetools.Folder.SUMMARY)
-    return cachetools.readdir(cache_dir)
+    return sorted((
+        dict(
+            filename=data.filename,
+            pe=data.contents['quoteSummary']['result'][0]['defaultKeyStatistics']['forwardPE']['raw'],
+        )
+        for data in _filecontents(cache_base, cachetools.Folder.SUMMARY)
+    ), key=lambda x: x['pe'])
