43031b36272844a50106d734be9ad1752e5d3e4e
Andrew Palmer <atp@sdf.org>
2020-05-08 00:06:19 -0500

    Add price to Yahoo API

--- stockprice/sources/yahoo.py
+++ stockprice/sources/yahoo.py
@@ -81,6 +81,16 @@ class api(object):
         }
         return jsonwebapi.get(**opts)
 
+    def price(ticker):
+        opts = {
+            'url': 'https://query1.finance.yahoo.com/v10/finance/quoteSummary/{ticker}'.format(
+                ticker=ensure_valid_ticker(ticker)),
+            'params': {
+                'modules': 'price'
+            },
+        }
+        return jsonwebapi.get(**opts)
+
     def quote_type(ticker):
         opts = {
             'url': 'https://query1.finance.yahoo.com/v1/finance/quoteType/{ticker}'.format(
