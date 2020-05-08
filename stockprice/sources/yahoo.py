8c3ae3a444b756626ada18337c2b3156df70811b
Andrew Palmer <atp@sdf.org>
2020-05-07 23:55:03 -0500

    Add earnings to Yahoo API

--- stockprice/sources/yahoo.py
+++ stockprice/sources/yahoo.py
@@ -71,6 +71,16 @@ class api(object):
         }
         return jsonwebapi.get(**opts)
 
+    def earnings(ticker):
+        opts = {
+            'url': 'https://query1.finance.yahoo.com/v10/finance/quoteSummary/{ticker}'.format(
+                ticker=ensure_valid_ticker(ticker)),
+            'params': {
+                'modules': 'earnings'
+            },
+        }
+        return jsonwebapi.get(**opts)
+
     def quote_type(ticker):
         opts = {
             'url': 'https://query1.finance.yahoo.com/v1/finance/quoteType/{ticker}'.format(
