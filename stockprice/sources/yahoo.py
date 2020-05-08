75636e611d3d4617db15298b278817fd97c2ae75
Andrew Palmer <atp@sdf.org>
2020-05-08 00:12:50 -0500

    Add calendar events to Yahoo API

--- stockprice/sources/yahoo.py
+++ stockprice/sources/yahoo.py
@@ -101,6 +101,16 @@ class api(object):
         }
         return jsonwebapi.get(**opts)
 
+    def calendar_events(ticker):
+        opts = {
+            'url': 'https://query1.finance.yahoo.com/v10/finance/quoteSummary/{ticker}'.format(
+                ticker=ensure_valid_ticker(ticker)),
+            'params': {
+                'modules': 'calendarEvents'
+            },
+        }
+        return jsonwebapi.get(**opts)
+
     def quote_type(ticker):
         opts = {
             'url': 'https://query1.finance.yahoo.com/v1/finance/quoteType/{ticker}'.format(
