e61eba78a4820586d2b01f6bd3332b5ef37b39f7
Andrew Palmer <atp@sdf.org>
2020-05-09 23:15:04 -0500

    Add data to risk report

--- stockprice/core/reports.py
+++ stockprice/core/reports.py
@@ -14,6 +14,20 @@ class Reports(object):
         debt = financial['totalDebt']
         return (debt - cash) / ev
 
+    def _debt_to_enterprise_val(self, ticker):
+        summary = self._raw.summary(ticker)
+        financial = self._raw.financial(ticker)
+        ev = summary['enterpriseValue']
+        debt = financial['totalDebt']
+        return debt / ev
+
+    def _cash_to_enterprise_val(self, ticker):
+        summary = self._raw.summary(ticker)
+        financial = self._raw.financial(ticker)
+        ev = summary['enterpriseValue']
+        cash = financial['totalCash']
+        return cash / ev
+
     def _short_pct(self, ticker):
         summary = self._raw.summary(ticker)
         return summary['shortPercentOfFloat']
@@ -46,6 +60,8 @@ class Reports(object):
         return {
             'ticker': ticker,
             'netDebtToEnterprise': self._net_debt_to_enterprise_val(ticker),
+            'debtToEnterprise': self._debt_to_enterprise_val(ticker),
+            'cashToEnterprise': self._cash_to_enterprise_val(ticker),
             'beta': self._beta(ticker),
             'priceToBook': self._pb(ticker),
             'shortPercent': self._short_pct(ticker),
