6b8c43f1091b982458650bc50bf3d3bb91ba7ff2
Andrew Palmer <atp@sdf.org>
2021-01-31 21:34:48 -0600

    Inline yahoo Chart data unwrapper

--- stockprice/models/rawdata.py
+++ stockprice/models/rawdata.py
@@ -1,7 +1,23 @@
+from datetime import datetime, timezone
 from ..sources import yahoo
 from ..docstore import DocumentStore
-from .yahoo.chart import Chart
 from .folder import Folder
+
+
+class Chart(object):
+    def __init__(self, data):
+        self._data = data
+
+    def get_items(self):
+        unwrapped_data = self._data['chart']['result'][0]
+        indicators = unwrapped_data['indicators']['quote'][0]
+        timestamps = (
+            datetime.fromtimestamp(ts).replace(tzinfo=timezone.utc).isoformat()
+            for ts in unwrapped_data['timestamp'])
+        return [
+            {k: v  for k, v in zip((*indicators.keys(), 'timestamp'), row)}
+            for row in zip(*indicators.values(), timestamps)
+        ]
 
 
 class transformations(object):
