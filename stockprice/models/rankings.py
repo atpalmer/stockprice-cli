891f2875df38f18141bc83ccd21624328975d6b6
Andrew Palmer <atp@sdf.org>
2021-02-04 16:11:51 -0600

    Break out Documents class

--- stockprice/models/rankings.py
+++ stockprice/models/rankings.py
@@ -2,6 +2,14 @@ from .folder import Folder
 from .rawdata import RawData
 
 
+class Documents(object):
+    def __init__(self, rawdata):
+        self._raw = rawdata
+
+    def summary(self):
+        return self._raw.documents(Folder.SUMMARY)
+
+
 def _sortby(docs, keys, *, sortkey=None, reverse=False):
     if sortkey is None:
         sortkey = keys[0]
@@ -19,29 +27,29 @@ def _sortby(docs, keys, *, sortkey=None, reverse=False):
 
 class Rankings(object):
     def __init__(self, cache_base):
-        self._raw = RawData(cache_base)
+        self._docs = Documents(RawData(cache_base))
 
     def pe(self):
         return _sortby(
-            self._raw.documents(Folder.SUMMARY),
+            self._docs.summary(),
             ['forwardPE', 'earningsQuarterlyGrowth'],
             sortkey='forwardPE')
 
     def ev_to_ebitda(self):
         return _sortby(
-            self._raw.documents(Folder.SUMMARY),
+            self._docs.summary(),
             ['enterpriseToEbitda', 'forwardPE'],
             sortkey='enterpriseToEbitda')
 
     def peg(self):
         return _sortby(
-            self._raw.documents(Folder.SUMMARY),
+            self._docs.summary(),
             ['pegRatio', 'forwardPE'],
             sortkey='pegRatio')
 
     def growth(self):
         return _sortby(
-            self._raw.documents(Folder.SUMMARY),
+            self._docs.summary(),
             ['earningsQuarterlyGrowth'],
             sortkey='earningsQuarterlyGrowth',
             reverse=True)
