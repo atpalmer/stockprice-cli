4d2c9a63d80dd3ce8aec2df87eacf14e9963255c
Andrew Palmer <atp@sdf.org>
2021-05-14 00:38:35 -0500

    Organize report

--- stockprice/models/reports.py
+++ stockprice/models/reports.py
@@ -34,20 +34,26 @@ class Reports(object):
     def essentials(self, ticker):
         return {
             'ticker': ticker,
-            'EbitdaToEv': self._ks.ebitda_to_ev(ticker),
-            'trailingPe': self._ks.trailing_pe(ticker),
-            'forwardPe': self._ks.forward_pe(ticker),
-            'priceToSales': self._ks.price_to_sales(ticker),
-            'priceToBook': self._ks.pb(ticker),
-            'earningsGrowth': self._ks.earnings_growth(ticker),
-            'revenueGrowth': self._ks.revenue_growth(ticker),
-            'netDebtToEnterprise': self._ks.net_debt_to_enterprise_val(ticker),
-            'cashToEnterprise': self._ks.cash_to_enterprise_val(ticker),
-            'beta': self._ks.beta(ticker),
-            'ROA': self._ks.roa(ticker),
-            'ROE': self._ks.roe(ticker),
-            'roeToPriceBook': self._ks.roe_pb(ticker),
-            'betaAdjRoeToPriceBook': self._ks.beta_adj_roe_pb(ticker),
-            'currentRatio': self._ks.current_ratio(ticker),
+            'valuation': {
+                'ebitdaToEv': self._ks.ebitda_to_ev(ticker),
+                'trailingPe': self._ks.trailing_pe(ticker),
+                'forwardPe': self._ks.forward_pe(ticker),
+                'priceToSales': self._ks.price_to_sales(ticker),
+                'priceToBook': self._ks.pb(ticker),
+                'beta': self._ks.beta(ticker),
+                'roeToPriceBook': self._ks.roe_pb(ticker),
+                'betaAdjRoeToPriceBook': self._ks.beta_adj_roe_pb(ticker),
+            },
+            'performance': {
+                'earningsGrowth': self._ks.earnings_growth(ticker),
+                'revenueGrowth': self._ks.revenue_growth(ticker),
+                'ROA': self._ks.roa(ticker),
+                'ROE': self._ks.roe(ticker),
+            },
+            'balanceSheet': {
+                'netDebtToEnterprise': self._ks.net_debt_to_enterprise_val(ticker),
+                'cashToEnterprise': self._ks.cash_to_enterprise_val(ticker),
+                'currentRatio': self._ks.current_ratio(ticker),
+            },
         }
 
